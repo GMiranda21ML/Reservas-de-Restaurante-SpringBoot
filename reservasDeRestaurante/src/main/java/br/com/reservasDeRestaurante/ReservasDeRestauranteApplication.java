aef40f68714028a032ccff93a85d71d4fd7c25cd
GMiranda21ML <gabriel.mucarbel.lima@gmail.com>
2025-07-13 21:18:28 -0300

    iniciando projeto

---
+++ reservasDeRestaurante/src/main/java/br/com/reservasDeRestaurante/ReservasDeRestauranteApplication.java
@@ -0,0 +1,13 @@
+package br.com.reservasDeRestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasDeRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasDeRestauranteApplication.class, args);
+	}
+
+}
---
+++ reservasDeRestaurante/src/test/java/br/com/reservasDeRestaurante/ReservasDeRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.reservasDeRestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasDeRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
