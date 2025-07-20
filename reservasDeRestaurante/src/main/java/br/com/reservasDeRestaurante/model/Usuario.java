28b4806c38fe88603a1f5b76b175d28b5f581b39
GMiranda21ML <gabriel.mucarbel.lima@gmail.com>
2025-07-20 17:02:26 -0300

    criando a tabela de usuarios

---
+++ reservasDeRestaurante/src/main/java/br/com/reservasDeRestaurante/model/Usuario.java
@@ -0,0 +1,17 @@
+package br.com.reservasDeRestaurante.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+    private String senha;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
