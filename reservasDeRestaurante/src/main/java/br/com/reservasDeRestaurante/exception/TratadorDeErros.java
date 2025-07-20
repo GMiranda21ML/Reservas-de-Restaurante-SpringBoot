690ba8d7e93e21d49d7ef46e277240643baf0c36
GMiranda21ML <gabriel.mucarbel.lima@gmail.com>
2025-07-20 20:25:52 -0300

    tratando erros

--- reservasDeRestaurante/src/main/java/br/com/reservasDeRestaurante/exception/TratadorDeErros.java
+++ reservasDeRestaurante/src/main/java/br/com/reservasDeRestaurante/exception/TratadorDeErros.java
@@ -2,9 +2,13 @@ package br.com.reservasDeRestaurante.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
@@ -14,4 +18,25 @@ public class TratadorDeErros {
     public ResponseEntity<String> tratarMesaNaoEncontrada(NoSuchElementException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Mesa não encontradda");
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> detalharErros(MethodArgumentNotValidException ex) {
+        Map<String, String> erros = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((erro) -> {
+            String campo = ((FieldError) erro).getField();
+            String mensagem = erro.getDefaultMessage();
+            erros.put(campo, mensagem);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+        Map<String, String> erro = new HashMap<>();
+        erro.put("erro", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+    }
+
 }
